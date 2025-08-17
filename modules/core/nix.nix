99ee6864da7efd24394fe193688909a750d3d1e1
coglinks <fairoosmohd2@gmail.com>
2025-08-17 17:27:04 +0200

    nix-old: various utils incomplete merge
    added tmux yazi
    
    applications substituted to preferred and keybind fixes
    
    bluetooth: enabled
    
    hyprland: gaps = 0, focus follows mouse
    
    rm ghostty & zen-browser, add firefox
    
    zsh: configured mkOrder & added yazi integration
    
    rm non-nixCats nvim
    
    hyprland keybind fixes, added missing pkg
    
    firefox: extension nixified setup
    
    nix system update
    
    git configured
    
    hyprland: added scripts
    
    hyprland: rm specific workspace rules
    
    firefox: config search engines & add extensions
    
    nix-old: misscellaneous settings fix
    
    nix-old: sddm config merge
    
    thunderbird: added
    
    zathura: nixified config

---
+++ modules/core/nix.nix
@@ -0,0 +1,17 @@
+{ pkgs, ... }:
+
+{
+# Disabled as it's not needed for now 
+  # this allows you to access `pkgs-stable` anywhere in your config
+  #_module.args.pkgs-stable = import inputs.nixpkgs-stable {
+  #  inherit (pkgs.stdenv.hostPlatform) system;
+  #  inherit (config.nixpkgs) config;
+  #};
+
+  system.autoUpgrade.enable = true;
+
+  nix.settings = {
+    max-jobs = 3;
+    cores = 4;
+  };
+}
