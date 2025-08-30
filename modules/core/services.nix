bd86cd3649acbcfa0cb4576cf46716f224ffe1ee
coglinks <fairoosmohd2@gmail.com>
2025-08-30 15:17:11 +0300

    auto update setup

--- modules/core/services.nix
+++ modules/core/services.nix
@@ -15,4 +15,39 @@
       gnome-settings-daemon
     ];
   };
+
+  systemd.services.pull-updates = {
+    description = "Pulls changes to system config";
+    restartIfChanged = false;
+    onSuccess = [ "rebuild.service" ];
+    startAt = "Sun 05:00";
+    path = [pkgs.git pkgs.openssh];
+    script = ''
+      test "$(git branch --show-current)" = "main"
+      git pull --ff-only
+    '';
+    serviceConfig = {
+      WorkingDirectory = "/etc/nixos";
+      User = "user-that-owns-the-repo";
+      Type = "oneshot";
+    };
+  };
+
+  systemd.services.rebuild = {
+    description = "Rebuilds and activates system config";
+    restartIfChanged = false;
+    path = [pkgs.nixos-rebuild pkgs.systemd];
+    script = ''
+      nixos-rebuild boot
+      booted="$(readlink /run/booted-system/{initrd,kernel,kernel-modules})"
+      built="$(readlink /nix/var/nix/profiles/system/{initrd,kernel,kernel-modules})"
+
+      if [ "''${booted}" = "''${built}" ]; then
+        nixos-rebuild switch
+      else
+        notify-send "reboot for kernal update"
+      fi
+    '';
+    serviceConfig.Type = "oneshot";
+  };
 }
