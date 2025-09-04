a90da9f312c810ae2369363cceb4014c2fc1a2f1
coglinks <fairoosmohd2@gmail.com>
2025-09-04 18:55:19 +0300

    hyprexpo: gesture change

--- modules/home/hyprland/plugins/hyprexpo.nix
+++ modules/home/hyprland/plugins/hyprexpo.nix
@@ -12,9 +12,9 @@
         bg_col = "rgb(a89984)";
         workspace_method = "first 1";
         enable_gesture = true; # laptop touchpad
-        gesture_fingers = 3;  # 3 or 4
+        gesture_fingers = 3; # 3 or 4
         gesture_distance = 300; # how far is the "max"
-        gesture_positive = true; # positive = swipe down. Negative = swipe up.
+        gesture_positive = false; # positive = swipe down. Negative = swipe up.
       };
     };
 
