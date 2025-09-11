5a841f8221240fab2bd6fd75d561cee8c7807ac3
coglinks <fairoosmohd2@gmail.com>
2025-09-11 12:31:02 +0300

    xremap: setup

--- flake.nix
+++ flake.nix
@@ -63,6 +63,8 @@
       url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    xremap-flake.url = "github:xremap/nix-flake";
   };
 
   outputs =
