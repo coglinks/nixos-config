7c8a53a97b926f5ecf9ba1de650471a9569e0f92
coglinks <fairoosmohd2@gmail.com>
2025-09-13 17:28:43 +0300

    university: added software

--- modules/home/packages/gui.nix
+++ modules/home/packages/gui.nix
@@ -16,6 +16,15 @@
     ## Office
     libreoffice
     gnome-calculator
+    ciscoPacketTracer8 # Go to https://www.netacad.com/resources/lab-downloads?courseLang=en-US to download CiscoPacketTracer822_amd64_signed.deb (If the file name is different, change it to as required by nix). Reason is cuz of:
+    /*
+      Unfortunately, we cannot download file CiscoPacketTracer822_amd64_signed.deb automatically.
+      > Please go to https://www.netacad.com to download it yourself, and add it to the Nix store
+      > using either
+      >   nix-store --add-fixed sha256 CiscoPacketTracer822_amd64_signed.deb
+      > or
+      >   nix-prefetch-url --type sha256 file:///path/to/CiscoPacketTracer822_amd64_signed.de
+    */
 
     ## Utility
     dconf-editor
