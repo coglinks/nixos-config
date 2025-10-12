6e2c3ecba1eccffb5229b97e6705d8ab72569a3e
coglinks <fairoosmohd2@gmail.com>
2025-10-12 21:48:26 +0300

    added root pkgs for basic root usage

--- modules/core/program.nix
+++ modules/core/program.nix
@@ -3,7 +3,13 @@
   programs.dconf.enable = true;
   programs.zsh.enable = true;
   programs.nano.enable = false;
-  programs.vim.enable = true;
+  programs.vim = {
+    enable = true;
+    defaultEditor = true;
+  };
+  programs.yazi = {
+    enable = true;
+  };
   programs.gnupg.agent = {
     enable = true;
     enableSSHSupport = true;
@@ -11,4 +17,11 @@
   };
   programs.nix-ld.enable = true;
   programs.nix-ld.libraries = with pkgs; [ ];
+  environment.systemPackages = with pkgs; [
+    wl-clipboard
+    refind
+    _7zz-rar
+    fd
+    ueberzugpp
+  ];
 }
