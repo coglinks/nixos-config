92c87d68ab49ca6c680083585887beb4c007b81d
coglinks <fairoosmohd2@gmail.com>
2025-10-12 07:57:28 +0300

    hosts/laptop: hardware-configuration update

--- hosts/laptop/hardware-configuration-extra.nix
+++ hosts/laptop/hardware-configuration-extra.nix
@@ -1,6 +1,6 @@
 { ... }:
 
 {
-  boot.initrd.luks.devices."swap".device = "/dev/disk/by-uuid/ac4f82cf-2f7a-4554-a2fd-3c1a4d421c62";
-  boot.resumeDevice = "/dev/dm-0";
+  boot.initrd.luks.devices."swap".device = "/dev/disk/by-uuid/4da29f8a-272e-4b96-bd6e-bae78ed5c1e3";
+  boot.resumeDevice = "/dev/disk/by-uuid/98fcee9c-36c5-40bf-b35e-5d8fb09f4be8";
 }
