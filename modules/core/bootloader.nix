98773d92f826fd1feeb07220820e5458f0772c44
coglinks <fairoosmohd2@gmail.com>
2025-10-12 21:47:48 +0300

    systemd: rm timeout

--- modules/core/bootloader.nix
+++ modules/core/bootloader.nix
@@ -2,6 +2,7 @@
 {
   boot.loader.systemd-boot.enable = true;
   boot.loader.systemd-boot.configurationLimit = 10;
+  boot.loader.timeout = 0;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.initrd.systemd.enable = true;
   boot.kernelPackages = pkgs.linuxPackages_zen;
