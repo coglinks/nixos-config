5c18da67ead9e35b308707b27358e057eb026ce7
coglinks <fairoosmohd2@gmail.com>
2025-10-15 21:13:01 +0300

    git: feature-sync-squashed-main setup

--- modules/home/packages/custom/default.nix
+++ modules/home/packages/custom/default.nix
@@ -3,6 +3,7 @@
   imports = [
     ./tokei-all.nix
     ./program-chooser/program-chooser.nix
+    ./feature-sync-squashed-source.nix
     ./nix-search/nix-search.nix # TUI to search nixpkgs
   ];
 }
