c57a0e28ca181e82236d9d64143e1fba8183677b
coglinks <fairoosmohd2@gmail.com>
2025-10-17 07:23:55 +0300

    added: devenv cachix

--- modules/core/nix.nix
+++ modules/core/nix.nix
@@ -8,7 +8,9 @@
   #  inherit (config.nixpkgs) config;
   #};
   nix.settings = {
-    max-jobs = 3;
-    cores = 4;
+    # max-jobs = 3;
+    # cores = 4;
+    extra-substituters = [ "https://devenv.cachix.org" ];
+    extra-trusted-public-keys = [ "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=" ];
   };
 }
