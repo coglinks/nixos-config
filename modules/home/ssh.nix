dfbfec49bea732d2e3c94bd1e0926285671b3608
coglinks <fairoosmohd2@gmail.com>
2025-11-12 12:18:25 +0300

    rm redundant

--- modules/home/ssh.nix
+++ modules/home/ssh.nix
@@ -3,33 +3,5 @@
   programs.ssh = {
     enable = true;
     enableDefaultConfig = false;
-
-    matchBlocks = {
-      github = {
-        host = "cog-github";
-        hostname = "github.com";
-        user = "git";
-        identityFile = "~/.ssh/id_github";
-        identitiesOnly = true;
-        addKeysToAgent = "yes";
-        controlMaster = "auto";
-        controlPath = "~/.ssh/control-%r@%h:%p";
-        controlPersist = "10m";
-      };
-      soft-serve = {
-        host = "soft-serve";
-        hostname = builtins.readFile "${inputs.secrets}/hosts/laptop";
-        user = username;
-        port = 23231;
-        identityFile = "~/.ssh/${username}";
-        identitiesOnly = true;
-        addKeysToAgent = "yes";
-        controlMaster = "auto";
-        controlPath = "~/.ssh/control-%r@%h:%p";
-        controlPersist = "10m";
-      };
-    };
   };
-
-  services.ssh-agent.enable = true;
 }
