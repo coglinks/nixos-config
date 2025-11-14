c96d5eb8ecdb9426c3bfcd30b603d45b264ba38d
coglinks <fairoosmohd2@gmail.com>
2025-11-14 06:46:45 +0300

    rm: feature-sync-squashed-source.nix

--- modules/home/packages/custom/default.nix
+++ modules/home/packages/custom/default.nix
@@ -3,7 +3,6 @@
   imports = [
     ./tokei-all.nix
     ./program-chooser/program-chooser.nix
-    ./feature-sync-squashed-source.nix
     ./nix-search/nix-search.nix # TUI to search nixpkgs
   ];
 }
